6c829e318f9a14061edd05db328927950ae62d0c
bastorz <damianalcalayy@gmail.com>
2023-03-31 21:10:29 +0100

    post and profile

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,19 +1,14 @@
-import { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  privateProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const profileRouter = createTRPCRouter({
   getUserByUsername: publicProcedure
     .input(z.object({ username: z.string() }))
-    .query(async ({ ctx, input }) => {
+    .query(async ({ input }) => {
       const [user] = await clerkClient.users.getUserList({
         username: [input.username],
       });
