ae84716cee8a2964df13e1bd88069ad169b0baad
bastorz <damianalcalayy@gmail.com>
2023-03-31 21:27:07 +0100

    post and profile

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -14,10 +14,22 @@ export const profileRouter = createTRPCRouter({
       });
 
       if (!user) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "User not found",
+        // if we hit here we need a unsantized username so hit api once more and find the user.
+        const users = await clerkClient.users.getUserList({
+          limit: 200,
         });
+        const user = users.find((user) =>
+          user.externalAccounts.find(
+            (account) => account.username === input.username
+          )
+        );
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user);
       }
 
       return filterUserForClient(user);
