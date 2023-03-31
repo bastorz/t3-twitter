47231f3bfcd97bebb78cdf57ce866b7974dd4d37
bastorz <damianalcalayy@gmail.com>
2023-03-31 21:04:24 +0100

    post and profile page working properly

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -14,7 +14,7 @@ const ProfileFeed = (props: { userId: string }) => {
 
   if (isLoading) return <LoadingPage />;
 
-  if (!data || data.length === 0) return <div>User has not posted</div>;
+  if (!data || data?.length === 0) return <div>User has not posted</div>;
 
   return (
     <div className="flex flex-col">
@@ -33,13 +33,13 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   return (
     <>
       <Head>
-        <title>{data.username ?? data.username}</title>
+        <title>{data?.username ?? data?.username}</title>
       </Head>
       <PageLayout>
         <div className="relative h-36 bg-slate-600">
           <Image
-            src={data.profilePicture}
-            alt={`${data.username ?? data.username}'s profile pic`}
+            src={data?.profilePicture}
+            alt={`${data?.username ?? data?.username}'s profile pic`}
             width={128}
             height={128}
             className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
@@ -47,10 +47,10 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         </div>
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${
-          data.username ?? data.username
+          data?.username ?? data?.username
         }`}</div>
         <div className="w-full border-b border-slate-400" />
-        <ProfileFeed userId={data.id} />
+        <ProfileFeed userId={data?.id} />
       </PageLayout>
     </>
   );
